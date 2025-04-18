bada95b5e3ec66e5ac528e5c213caa74eb0e62e4
Alberto Moretti <moretti919@gmail.com>
2025-04-19 00:01:39 +0200

    first boot working

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ QEMU=qemu-system-riscv32
 
 # Path to clang and compiler flags
 CC=clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
